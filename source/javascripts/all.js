0a595202e1ec8e8a3035d5ab14811d9378db22d2
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-12-30 18:07:41 -0500

    adding link tracking

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -16,4 +16,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-58107731-1', 'auto');
+ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
