d063db5eba831c57a245afc1fe653b5cab702745
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-02-17 22:21:15 -0500

    force gallery to be centered

--- source/javascripts/galleria.portfolio.js
+++ source/javascripts/galleria.portfolio.js
@@ -13,4 +13,23 @@ $(function(){
     fullscreenDoubleTap: false,
   });
   Galleria.run('.galleria');
+  Galleria.ready(function() {
+    var thumbcont=this.$('thumbnails-container'),
+    thumb=this.$('thumbnails-list'),
+    thumbw,thumbmarg,left,contw,
+    center=function(){
+      contw=thumbcont.width();
+      left=((contw-Math.floor(contw/thumbw)*thumbw)/2+thumbmarg)+'px';
+      thumb.css('left',left);
+    };
+    this.bind('thumbnail', function(e) {
+      if (!thumbw) {
+        var thumbp=$(e.thumbTarget).parent();
+        thumbw=thumbp.outerWidth(true);
+        thumbmarg=(parseFloat(thumbp.css('margin-right'))-parseFloat(thumbp.css('margin-left')))/2;
+        center();
+      }
+    });
+    $(window).on('resize',center);
+  });
 });
